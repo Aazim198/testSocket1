c1ba41b7e6c507f2914428a950b0d1ef8fc3f5ce
Syed Mohmmad Aazam <mohmmad.aazam@grazitti.local>
2023-01-04 17:19:22 +0530

    rooms are working

--- index.js
+++ index.js
@@ -16,20 +16,32 @@ app.get('/',(req,res)=>{
 
 io.on('connection',(socket)=>{
     console.log(`connection started with id:  ${socket.id}`);    
-    socket.on('sendMesage',(message)=>{
+    socket.on('sendMesage',(message,room)=>{
         // io.emit('rxmsg',message);
-        socket.broadcast.emit('rxmsg',message);
+        // socket.broadcast.emit('rxmsg',message);
+        if(room !=""){
+            socket.to(room).emit('rxmsg',message);
+        }else{
+            socket.broadcast.emit('rxmsg',message);
+        }
     })
-    
+    socket.on('joinTheGroup',(room)=>{
+        socket.join(room);
+        // socket.in(room).emit('You are in the room',room);
+    })
+    // socket.on('sendMesage',(message,room)=>{
+    //     if(room){
+    //         socket.to(room).emit(message);
+    //     }else{
+    //         socket.broadcast.emit('rxmsg',message);
+    //     }
+    // })
+
     // socket.emit('receiveMessage','Hello from server')
     socket.on('disconnect',()=>{
         console.log(`connection closed!!`);    
-    })
-
-   
+    })   
 })
-
-
 
 // app.listen(port,()=>{
 //     console.log('Connected on port',port);
